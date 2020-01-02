0981fed5f1be0a3853b8977eac2321d2d1f72c8c
Prashant.Garg <Prashant.Garg@ie.ey.com>
2020-01-02 10:40:55 +0530

    storing key value within key value in Key Vault

--- az_keyvault_operations.py
+++ az_keyvault_operations.py
@@ -7,6 +7,11 @@ from azure.identity import DefaultAzureCredential
 keyVaultName = os.environ["KEY_VAULT_NAME"]
 KVUri = "https://" + keyVaultName + ".vault.azure.net"
 
+key_value_pair = {}
+
+key_value_pair['key1'] = 'value1'
+key_value_pair['key2'] = 'value2'
+
 credential = DefaultAzureCredential()
 client = SecretClient(vault_url=KVUri, credential=credential)
 
@@ -14,7 +19,7 @@ secretName = str(uuid.uuid4())
 secretValue = "secretValue"
 
 # Set secret
-client.set_secret(secretName, secretValue)
+client.set_secret(secretName, key_value_pair)
 
 # Retrieve secret
 retrieved_secret = client.get_secret(secretName)
